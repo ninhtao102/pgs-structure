37a87b530fbebe1f1aba893c572a72f289d88bb9
NinhTao <ninh.tao.102@gmail.com>
2022-02-09 17:48:08 +0700

    change LoginPage

--- src/modules/auth/pages/LoginPage.tsx
+++ src/modules/auth/pages/LoginPage.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useState } from 'react';
+import { ILoginParams } from '../../../models/auth';
 import LoginForm from '../components/LoginForm';
 import logo from '../../../logo-420-x-108.png';
-import { ILoginParams } from '../../../models/auth';
 import { useDispatch } from 'react-redux';
 import { ThunkDispatch } from 'redux-thunk';
 import { AppState } from '../../../redux/reducer';
@@ -9,12 +10,13 @@ import { Action } from 'redux';
 import { fetchThunk } from '../../common/redux/thunk';
 import { API_PATHS } from '../../../configs/api';
 import { RESPONSE_STATUS_SUCCESS } from '../../../utils/httpResponseCode';
+// import { replace } from 'lodash';
+import { replace } from 'connected-react-router';
+import { ROUTES } from '../../../configs/routes';
+import { getErrorMessageResponse } from '../../../utils';
 import { setUserInfo } from '../redux/authReducer';
 import Cookies from 'js-cookie';
 import { ACCESS_TOKEN_KEY } from '../../../utils/constants';
-import { ROUTES } from '../../../configs/routes';
-import { replace } from 'connected-react-router';
-import { getErrorMessageResponse } from '../../../utils';
 
 const LoginPage = () => {
   const dispatch = useDispatch<ThunkDispatch<AppState, null, Action<string>>>();
@@ -27,38 +29,39 @@ const LoginPage = () => {
       setLoading(true);
 
       const json = await dispatch(
-        fetchThunk(API_PATHS.signIn, 'post', { email: values.email, password: values.password }),
+        fetchThunk(API_PATHS.signIn, 'post', {email: values.email, password: values.password}),
       );
 
       setLoading(false);
 
       if (json?.code === RESPONSE_STATUS_SUCCESS) {
-        dispatch(setUserInfo(json.data));
-        Cookies.set(ACCESS_TOKEN_KEY, json.data.token, { expires: values.rememberMe ? 7 : undefined });
+        dispatch(setUserInfo(json.data)); //dispatch action set user data
+
+        //Set login token to Cookie when user choose remember me
+        Cookies.set(ACCESS_TOKEN_KEY, json.data.token, {expires: values.rememberMe ? 7 : undefined});
+        
+        //Replace current path to home page 
+
         dispatch(replace(ROUTES.home));
         return;
       }
 
       setErrorMessage(getErrorMessageResponse(json));
-    },
-    [dispatch],
-  );
+  }, [dispatch]);
 
   return (
-    <div
-      className="container"
-      style={{
-        height: '100vh',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'column',
-      }}
-    >
-      <img src={logo} alt="" style={{ maxWidth: '250px', margin: '32px' }} />
-
-      <LoginForm onLogin={onLogin} loading={loading} errorMessage={errorMessage} />
-    </div>
+  <div
+  className='container'
+  style={{
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column'
+  }}>
+    <img src={logo} alt="" style={{maxWidth: '250px', margin: '32px'}} />
+    <LoginForm onLogin={onLogin} loading={false} errorMessage={errorMessage} />
+  </div>
   );
 };
 

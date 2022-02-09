a6391dcb61fe170f345e18b5cf35600568fa5d8e
NinhTao <ninh.tao.102@gmail.com>
2022-02-09 17:50:20 +0700

    add register interface

--- src/models/auth.ts
+++ src/models/auth.ts
@@ -8,3 +8,23 @@ export interface ILoginValidation {
   email: string;
   password: string;
 }
+
+export interface IRegisterParams {
+  email: string,
+  password: string,
+  repeatPassword: string,
+  name: string,
+  gender: string,
+  region: number,
+  state: number,
+}
+
+export interface IRegisterValidation {
+  email: string,
+  password: string,
+  repeatPassword: string,
+  name: string,
+  gender: string,
+  region: string | number,
+  state: string | number,
+}

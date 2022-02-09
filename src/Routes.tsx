494e79e3e3057a92d3999ecc5cf57b8b1aad4b6e
NinhTao <ninh.tao.102@gmail.com>
2022-02-09 17:49:15 +0700

    add register route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -6,6 +6,7 @@ import ProtectedRoute from './modules/common/components/ProtectedRoute';
 const HomePage = lazy(() => import('./modules/home/pages/HomePage'));
 const ContactPage = lazy(() => import('./modules/home/pages/ContactPage'));
 const LoginPage = lazy(() => import('./modules/auth/pages/LoginPage'));
+const RegisterPage = lazy(() => import('./modules/register/pages/RegisterPage'));
 
 interface Props {}
 
@@ -16,6 +17,7 @@ export const Routes = (props: Props) => {
     <Suspense fallback={<div>Loading.....</div>}>
       <Switch location={location}>
         <Route path={ROUTES.login} component={LoginPage} />
+        <Route path={ROUTES.register} component={RegisterPage} />
         <ProtectedRoute path={ROUTES.home} component={HomePage} />
         <Route path={ROUTES.contact} component={ContactPage} />
 

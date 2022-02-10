62f6bee6426b67b682823675f656106e4e712b09
NinhTao <ninh.tao.102@gmail.com>
2022-02-10 17:48:34 +0700

    get and render State data

--- src/modules/register/pages/RegisterPage.tsx
+++ src/modules/register/pages/RegisterPage.tsx
@@ -1,29 +1,98 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import logo from '../../../logo-420-x-108.png';
 import { IRegisterParams } from '../../../models/auth';
 import RegisterForm from '../components/RegisterForm'
+import { useDispatch } from 'react-redux';
+import { ThunkDispatch } from 'redux-thunk';
+import { AppState } from '../../../redux/reducer';
+import { Action } from 'redux';
+import { fetchThunk } from '../../common/redux/thunk';
+import { API_PATHS } from '../../../configs/api';
+import { RESPONSE_STATUS_SUCCESS } from '../../../utils/httpResponseCode';
+import { replace } from 'connected-react-router';
+import { ROUTES } from '../../../configs/routes';
+import { getErrorMessageResponse } from '../../../utils';
 
 const RegisterPage = () => {
+    const dispatch = useDispatch<ThunkDispatch<AppState, null, Action<string>>>();
     const [loading, setLoading] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
+    const [locations, setLocations] = useState([]);
+
+    const getLocation = React.useCallback(async () => {
+        setLoading(true)
+
+        const json = await dispatch(fetchThunk(API_PATHS.getLocation, 'get'))
+
+        setLoading(false)
+
+        if (json?.code === RESPONSE_STATUS_SUCCESS) {
+            setLocations(json.data)
+            return 
+        }
+    }, [dispatch])
+
+    useEffect(() => {
+    getLocation()
+    }, [getLocation])
 
     const onRegister = React.useCallback(
         async (values: IRegisterParams) => {
-            console.log(values)
-    }, []);
+            setErrorMessage('');
+            setLoading(true);
+
+            const json = await dispatch(
+                fetchThunk(
+                    API_PATHS.signUp, 'post', {
+                        email: values.email, 
+                        password: values.password,
+                        repeatPassword: values.repeatPassword,
+                        name: values.name,
+                        gender: values.gender,
+                        region: values.region,
+                        state: values.state
+                    }
+                ),
+            );
+
+            setLoading(false);
+
+            if (json?.code === RESPONSE_STATUS_SUCCESS) {
+                console.log(json.data)
+                alert('Chúc mừng bạn đã đăng ký thành công')
+                dispatch(replace(ROUTES.login));
+                return;
+            }
+
+            setErrorMessage(getErrorMessageResponse(json));
+    }, [dispatch]);
+
+    const fetchState = React.useCallback(async ( pid: number ) => {
+        if (pid) {
+            const resp = await dispatch(fetchThunk(`${API_PATHS.getLocation}?pid=${pid}`,'get'))
+            if (resp.code === RESPONSE_STATUS_SUCCESS) {
+                console.log(resp)
+                return resp.data
+            }
+            console.log(getErrorMessageResponse(resp))
+            return resp
+        }
+    },
+    [dispatch],
+    )
+    
 
     return (
         <div
         className='container'
         style={{
-            height: '100vh',
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
             flexDirection: 'column'
         }}>
             <img src={logo} alt="" style={{maxWidth: '250px', margin: '32px'}} />
-            <RegisterForm onRegister={onRegister} loading={loading} errorMessage={errorMessage} />
+            <RegisterForm onRegister={onRegister} loading={loading} errorMessage={errorMessage} locations={locations} city={fetchState} />
         </div>
     )
 }

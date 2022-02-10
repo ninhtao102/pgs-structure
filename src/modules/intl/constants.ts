90a46921d6e9971b9ab13b5715576dde267e346c
NinhTao <ninh.tao.102@gmail.com>
2022-02-10 17:42:51 +0700

    add Gender constants

--- src/modules/intl/constants.ts
+++ src/modules/intl/constants.ts
@@ -1,1 +1,15 @@
 export const LS_LANG = 'lang';
+export const GENDER = [
+    {
+        label: 'Nam',
+        value: 'male'
+    },
+    {
+        label: 'Nữ',
+        value: 'female'
+    },
+    {
+        label: 'Khác',
+        value: 'other'
+    }
+]
